9f401abf824f98fa9448e9a84cc38f7566a14534
burtk33 <burtk33@gmail.com>
2021-06-09 18:32:15 -0400

    solving routes issue

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use(routes);
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/google_book_search",
